f0056d7b044eb916ca3b5fffc90a271909c69216
David Schurer <david.schurer@yahoo.com>
2024-05-31 21:48:36 -0700

    updated the welcome page

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -12,9 +12,13 @@ function WelcomePage() {
     return (
         <div className={styles.welcomeContainer}>
             <div className={styles.welcomeBox}>
-                <h2 className={styles.welcomeHeader}>Welcome!</h2>
-                <p className={styles.welcomeMessage}>We are glad to have you at the Parking Reservation Website.</p>
+                <h2 className={styles.welcomeHeader}>Welcome to UW Bothell Parking Reservation!</h2>
+                <p className={styles.subHeading}><strong>Reserve Your Spot Today</strong></p>
+                <p className={styles.welcomeMessage}>
+                    Welcome to the UW Bothell Parking Reservation System! It's your portal to hassle-free parking on campus. Easily reserve your spot in any of our campus garages or lots, manage your reservations, and plan your campus visit with confidence.
+                </p>
                 <button className={styles.continueButton} onClick={handleContinueClick}>Continue</button>
+                <p className={styles.developedBy}>Developed by David Schurer and Shivam Bakshi</p>
             </div>
         </div>
     );
