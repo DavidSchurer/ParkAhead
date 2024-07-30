f562a12fe4978f65c41436db2af8c0af29857655
David Schurer <david.schurer@yahoo.com>
2024-07-29 18:05:50 -0700

    updated firebase trigger function

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,25 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 admin.initializeApp();
 
 exports.deleteReservation = functions.firestore
-    .document('reservations/{reservationId}')
+    .document("reservations/{reservationId}")
     .onWrite((change, context) => {
-        const reservation = change.after.data();
+      const reservation = change.after.data();
 
-        const currentDate = newDate();
-        const reservationDate = reservation.date.toDate();
-        const reservationEndTime = new Date(reservationDate.getTime() + reservation.endTime.toMillis());
-        if (reservationDate <= currentDate && reservationEndTime <= currentDate) {
-            return change.after.ref.delete();
-        }
-
+      if (!reservation) {
         return null;
+      }
+
+      const currentDate = new Date();
+      const reservationDate = reservation.date.toDate();
+      const reservationEndTime = new Date(
+          reservationDate.getTime() + reservation.endTime.toMillis());
+
+      if (reservationEndTime <= currentDate) {
+        return admin.firestore().doc(context.params.reservationId).delete();
+      }
+
+      return null;
     });
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
