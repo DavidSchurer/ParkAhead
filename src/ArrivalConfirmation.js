161ef8acc6455f57a6bdf5bc3980af09bc19e0bd
David Schurer <david.schurer@yahoo.com>
2024-08-01 00:34:17 -0700

    arrival confirmation page now starts at the reservation start time

--- src/ArrivalConfirmation.js
+++ src/ArrivalConfirmation.js
@@ -3,76 +3,151 @@ import { db, auth } from './firebase'; // Make sure to import your Firebase setu
 import { collection, getDocs, where, query, doc, updateDoc, deleteDoc, getDoc } from 'firebase/firestore';
 import styles from './ArrivalConfirmation.module.css';
 
+const TIMER_MS = 15 * 1000;
+
+const militaryTimeComponents = timeString=>{
+  const [time, modifier] = timeString.split(/(AM|PM)/i);
+  let [hours, minutes] = time.split(':').map(Number);
+
+  if(modifier.toUpperCase() === 'PM' && hours < 12){
+      hours += 12;
+  }
+  if(modifier.toUpperCase() === 'AM' && hours === 12){
+      hours = 0;
+  }
+
+  return [hours, minutes];
+}
+
 const ArrivalConfirmation = () => {
   const [reservations, setReservations] = useState([]);
   const [showConfirmation, setShowConfirmation] = useState(false);
-  const [countdown, setCountdown] = useState(15 * 1000);
+  const [countdown, setCountdown] = useState(TIMER_MS);
   const [timerActive, setTimerActive] = useState(true);
 
+  const countdownIntervalRef = React.useRef();
+
   useEffect(() => {
-    const fetchReservations = async () => {
-      try {
-        const user = auth.currentUser;
-        if (user) {
-          const reservationsQuery = query(
-            collection(db, 'reservations'),
-            where('userEmail', '==', user.email)
-          );
-          const querySnapshot = await getDocs(reservationsQuery);
 
-          const reservationsData = [];
-          querySnapshot.forEach((doc) => {
-            reservationsData.push({
-              id: doc.id,
-              ...doc.data(),
-              isConfirmed: doc.data().isConfirmed || false,
-              reservationExtended: doc.data().reservationExtended || false,
-            });
-          });
-          setReservations(reservationsData);
-
-          const anyConfirmed = reservationsData.some(reservation => reservation.isConfirmed);
-          if (anyConfirmed) {
-            setTimerActive(false);
-          }
-        }
-      } catch (error) {
-        console.log('Error fetching reservations:', error);
-      }
-    };
-
-    fetchReservations();
-
-    const savedEndTime = localStorage.getItem('countdownEndTime');
-    if (savedEndTime) {
-      const endTime = parseInt(savedEndTime, 10);
-      const now = new Date().getTime();
-      const remainingTime = Math.max(endTime - now, 0);
-      setCountdown(remainingTime);
+    if (window.ALREADY_RAN) {
+      return;
     }
+    window.ALREADY_RAN = true;
 
-    const countdownInterval = setInterval(() => {
-      if (timerActive) {
-        setCountdown(prevCountdown => {
-          if (prevCountdown <= 1000) {
-            clearInterval(countdownInterval);
-            handleTimerEnd();
-            return 0;
+    let delayTimeout;
+    console.log('test');
+    const processReservations = async()=>{
+      const fetchReservation = async () => {
+        try {
+          const user = auth.currentUser;
+          if (user) {
+            const reservationsQuery = query(
+              collection(db, 'reservations'),
+              where('userEmail', '==', user.email)
+            );
+            const querySnapshot = await getDocs(reservationsQuery);
+  
+            const reservationsData = [];
+            querySnapshot.forEach((doc) => {
+              reservationsData.push({
+                id: doc.id,
+                ...doc.data(),
+                isConfirmed: doc.data().isConfirmed || false,
+                reservationExtended: doc.data().reservationExtended || false,
+              });
+            });
+            setReservations(reservationsData);
+  
+            if(reservationsData.length===0) return -1;
+  
+            const theDateAndTime = reservationsData[0]?.date.toDate();
+            const theStartTime = reservationsData[0]?.startTime;
+            const startTimeComponents = militaryTimeComponents(theStartTime);
+            theDateAndTime.setHours(startTimeComponents[0], startTimeComponents[1], 0, 0);
+  
+            const anyConfirmed = reservationsData.some(reservation => reservation.isConfirmed);
+            if (anyConfirmed) {
+              setTimerActive(false);
+              return -1;
+            }
+  
+            return theDateAndTime;
           }
-          const newCountdown = prevCountdown - 1000;
-          localStorage.setItem('countdownEndTime', new Date().getTime() + newCountdown);
-          return newCountdown;
-        });
+        } catch (error) {
+          console.log('Error fetching reservations:', error);
+        }
+      };
+  
+      const theDateAndTime = await fetchReservation();
+      if (theDateAndTime === -1) {
+        return;
       }
-    }, 1000);
+  
+      const commenceTimer = (nextRemainingTime) => {
+        console.log('Hello');
+        if(nextRemainingTime<=0) return;
+  
+        clearInterval(countdownIntervalRef.current);
+        countdownIntervalRef.current = setInterval(() => {
+          if (timerActive) {
+            setCountdown(prevCountdown => {
+              if (prevCountdown <= 1000) {
+                clearInterval(countdownIntervalRef.current);
+                handleTimerEnd();
+                return 0;
+              }
+              const newCountdown = prevCountdown - 1000;
+              localStorage.setItem('countdownEndTime', new Date().getTime() + newCountdown);
+              return newCountdown;
+            });
+          }
+        }, 1000);
+  
+      };
+  
+      const savedEndTime = localStorage.getItem('countdownEndTime');
+      const now = new Date().getTime();
+      if (savedEndTime) {
+        const endTime = parseInt(savedEndTime, 10);
+        const remainingTime = Math.max(endTime - now, 0);
+        if (remainingTime <= 0) {
+          localStorage.removeItem('countdownEndTime');
+        } else {
+          setCountdown(remainingTime);
+          commenceTimer(remainingTime);
+          console.log('1');
+          return;
+        }
+      }
+        if ( theDateAndTime <= now) {
+          const timeDif = now - theDateAndTime;
+          const remainingTime = TIMER_MS - timeDif;
+          setCountdown(remainingTime);
+          commenceTimer(remainingTime);
+          console.log('2');
+        } else {
+          const delay = theDateAndTime - now;
+          clearTimeout(delayTimeout);
+          delayTimeout = setTimeout(() => {
+            setCountdown(TIMER_MS);
+            commenceTimer(TIMER_MS);
+            console.log('3');
+          }, delay);
+        }
+  
+      }
+
+    processReservations();
 
     return () => {
-      clearInterval(countdownInterval);
+      window.ALREADY_RAN = false;
+      clearInterval(countdownIntervalRef.current);
+      clearTimeout(delayTimeout);
       if (!timerActive) {
         localStorage.removeItem('countdownEndTime');
       }
     };
-  }, [timerActive]);
+  }, []);
 
   const handleTimerEnd = async () => {
     if (timerActive) {
@@ -96,11 +171,14 @@ const ArrivalConfirmation = () => {
         console.error('Error deleting reservations:', error);
       }
       setTimerActive(false);
+      localStorage.removeItem('countdownEndTime');
     }
   };
 
   const handleConfirmArrival = async (reservationId) => {
     setTimerActive(false);
+    clearInterval(countdownIntervalRef.current);
+    localStorage.removeItem('countdownEndTime');
 
     try {
       const reservationDocRef = doc(db, 'reservations', reservationId);
